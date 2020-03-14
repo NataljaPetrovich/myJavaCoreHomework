6270f35316fb7067d8fead1f13b9674b81f6f229
NataljaPetrovich <61056228+NataljaPetrovich@users.noreply.github.com>
2020-03-14 19:24:54 +0300

    task 29

--- src/main/java/com/homework/lesson10/task29/Main.java
+++ src/main/java/com/homework/lesson10/task29/Main.java
@@ -1,4 +1,13 @@
 package main.java.com.homework.lesson10.task29;
 
 public class Main {
+    public static void main(String[] args) {
+        String txt = "Old MacDonald had a farm Ee i ee i o And on his farm he had some cows Ee i ee i oh " +
+                "With a moo-moo here And a moo-moo there Here a moo, there a moo Everywhere a moo-moo " +
+                "Old MacDonald had a farm Ee i ee i o Old MacDonald had a farm Ee i ee i o";
+        FrequencyDictionary mcdonald = new FrequencyDictionary(txt);
+        mcdonald.showFrequences();
+        mcdonald.showNumOfUse();
+    }
+
 }
