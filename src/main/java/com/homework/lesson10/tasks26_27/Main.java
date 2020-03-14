4b92339a1f78b0f078a3c4a0d05a6c69da0d6083
NataljaPetrovich <61056228+NataljaPetrovich@users.noreply.github.com>
2020-03-14 19:24:54 +0300

    task 29

--- src/main/java/com/homework/lesson10/tasks26_27/Main.java
+++ src/main/java/com/homework/lesson10/tasks26_27/Main.java
@@ -4,9 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Journal myClass = new Journal("7F", 12);
         System.out.println(myClass);
+
+        //Task26
         myClass.removeBadMarks();
+
         myClass.setNewMarks();
         System.out.println(myClass);
+
+        //Task27
         myClass.removeRepeating();
     }
 }
