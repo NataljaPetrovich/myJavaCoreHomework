6b46af7ddc508f6915e6c755b8b25841639203ee
NataljaPetrovich <61056228+NataljaPetrovich@users.noreply.github.com>
2020-03-14 20:10:35 +0300

    task 29 corrected javaDocs

--- src/main/java/com/homework/lesson10/task29/Main.java
+++ src/main/java/com/homework/lesson10/task29/Main.java
@@ -6,7 +6,7 @@ public class Main {
                 "With a moo-moo here And a moo-moo there Here a moo, there a moo Everywhere a moo-moo " +
                 "Old MacDonald had a farm Ee i ee i o Old MacDonald had a farm Ee i ee i o";
         FrequencyDictionary mcdonald = new FrequencyDictionary(txt);
-        mcdonald.showFrequences();
+        mcdonald.showFrequency();
         mcdonald.showNumOfUse();
     }
 }
