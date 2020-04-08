0fe6072770416848cb0f1e65687960fecd4cf016
NataljaPetrovich <61056228+NataljaPetrovich@users.noreply.github.com>
2020-04-08 23:38:00 +0300

    task #1 (задача про кассы)

---
+++ src/main/java/com/homework/lesson15_16/Customer.java
@@ -0,0 +1,42 @@
+package main.java.com.homework.lesson15_16;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class Customer implements Runnable {
+    private int number;
+    private ArrayList<String> goodsToBuy;
+    static int count = 0;
+
+    public Customer() {
+        count++;
+        number = count;
+        goodsToBuy = new ArrayList<>();
+    }
+
+    void putGoodsInBasket(ArrayList<String> goodsInShop) {
+        Random random = new Random();
+        int numOfGoods = random.nextInt(7);
+        while (numOfGoods >= 0) {
+            int i = random.nextInt(goodsInShop.size());
+            goodsToBuy.add(goodsInShop.get(i));
+            numOfGoods--;
+        }
+    }
+
+    void showGoodsInBasket() {
+        System.out.println(goodsToBuy);
+    }
+
+    @Override
+    public void run() {
+        synchronized (goodsToBuy) {
+            System.out.println("Cuctomer " + number);
+            for (String s : goodsToBuy) {
+                System.out.print(s + "; ");
+            }
+            System.out.println( "Всего товаров: " + goodsToBuy.size());
+        }
+    }
+}
---
+++ src/main/java/com/homework/lesson15_16/Shop.java
@@ -0,0 +1,30 @@
+package main.java.com.homework.lesson15_16;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class Shop {
+    ArrayList<String> goodsInShop;
+    ExecutorService cashRegisters;
+
+    Shop(int numberOfCashRegisters, String... goods) {
+        cashRegisters = Executors.newFixedThreadPool(numberOfCashRegisters);
+        goodsInShop = (ArrayList<String>) Arrays.stream(goods).collect(Collectors.toList());
+    }
+
+    public void serveAllCustomers(Customer... customers) {
+        for (Customer c : customers) {
+            cashRegisters.execute(c);
+            try {
+                cashRegisters.awaitTermination(1000, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
