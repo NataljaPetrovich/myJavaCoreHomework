4b695070fe4f44553fa471449986822937161cfb
NataljaPetrovich <61056228+NataljaPetrovich@users.noreply.github.com>
2020-04-14 21:38:18 +0300

    Address Validator shorter version

--- src/main/java/com/homework/lesson17/Main.java
+++ src/main/java/com/homework/lesson17/Main.java
@@ -1,10 +1,7 @@
 package main.java.com.homework.lesson17;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter your address: ");
-        System.out.println(AddressValidator.addressIsValid(new Scanner(System.in).nextLine()));
+        System.out.println(AddressValidator.addressIsValid(AddressValidator.requestAddress()));
     }
 }
