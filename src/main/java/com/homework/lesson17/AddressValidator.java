881b2652e74a8190c5159e2ab663b021531dbd75
NataljaPetrovich <61056228+NataljaPetrovich@users.noreply.github.com>
2020-04-14 21:17:30 +0300

    Address Validator #2

--- src/main/java/com/homework/lesson17/AddressValidator.java
+++ src/main/java/com/homework/lesson17/AddressValidator.java
@@ -4,7 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AddressValidator {
-    private static final String regex = " *(E|D|R) *([0-9] *){12}";
+    //private static final String regex = " *(E|D|R) *([0-9] *){12}";
+    private static final String regex = "\\s*[EDR]\\s*([0-9]\\s*){12}";
 
     public static boolean addressIsValid(String address){
         Pattern pattern = Pattern.compile(regex);
