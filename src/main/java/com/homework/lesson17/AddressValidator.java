b6bdee41a6a0514176de1d290dd60ee943f3de02
NataljaPetrovich <61056228+NataljaPetrovich@users.noreply.github.com>
2020-04-14 21:30:14 +0300

    Address Validator shorter version

--- src/main/java/com/homework/lesson17/AddressValidator.java
+++ src/main/java/com/homework/lesson17/AddressValidator.java
@@ -1,18 +1,11 @@
 package main.java.com.homework.lesson17;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AddressValidator {
-    //private static final String regex = " *(E|D|R) *([0-9] *){12}";
     private static final String regex = "\\s*[EDR]\\s*([0-9]\\s*){12}";
 
-    public static boolean addressIsValid(String address){
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(address);
-        if(matcher.matches()){
-          return true;
-        }
-        return false;
+    public static boolean addressIsValid(final String address) {
+        return Pattern.matches(regex, address);
     }
 }
--- src/main/java/com/homework/lesson17/Main.java
+++ src/main/java/com/homework/lesson17/Main.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Enter your address: ");
-        Scanner scanner = new Scanner(System.in);
-        String address = scanner.nextLine();
-        System.out.println(AddressValidator.addressIsValid(address));
+        System.out.println(AddressValidator.addressIsValid(new Scanner(System.in).nextLine()));
     }
 }
